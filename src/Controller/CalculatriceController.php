3de77d77771f8e5a97020e517bf9d0166e5474da
OnlyDukeTV <onlyduketv@gmail.com>
2022-03-15 10:25:45 +0100

    Hello

---
+++ src/Controller/CalculatriceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CalculatriceController extends AbstractController
+{
+    /**
+     * @Route("/calculatrice", name="app_calculatrice")
+     */
+    public function index(): Response
+    {
+        return $this->render('calculatrice/index.html.twig', [
+            'controller_name' => 'CalculatriceController',
+        ]);
+    }
+}
