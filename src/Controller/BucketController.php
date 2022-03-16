72fadbe8b913549b9ca50630e29ee81128560fe6
OnlyDukeTV <onlyduketv@gmail.com>
2022-03-16 16:49:26 +0100

    Commit and push

--- src/Controller/BucketController.php
+++ src/Controller/BucketController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Offres;
+use App\Repository\ClientsRepository;
+use App\Repository\OffresRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,7 +18,7 @@ class BucketController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('bucket/reservation.html.twig', [
+        return $this->render('bucket/index.html.twig', [
             'controller_name' => 'BucketController',
         ]);
     }
@@ -21,10 +26,39 @@ class BucketController extends AbstractController
     /**
      * @Route("/main", name="app_aboutUs")
      */
-    public function aboutUs(): Response
+    public function aboutUs(EntityManagerInterface $em, Request $request): Response
     {
+        if ($request->getMethod() == "POST") {
+
+            $nom = $request->request->get("nom");
+            $duree = $request->request->get("duree");
+            $prix = $request->request->get("prix");
+
+
+            // CREATION DE L'OBJET
+            $offre = new Offres();
+            $offre->setNom($nom)
+                ->setDuree($duree)
+                ->setPrix($prix);
+
+            //AJOUT DANS BDD
+            $em->persist($offre);
+            $em->flush();
+        }
+
         return $this->render('main/offres.html.twig', [
             'controller_name' => 'BucketController',
         ]);
+
+    }
+
+    /**
+     * @Route("/main",name="app_aboutUs")
+     */
+    public function listeClient(OffresRepository $offreRepo): Response
+    {
+        $offres = $offreRepo->findAll();
+        dump($offres);
+        return $this->render("main/offres.html.twig",compact("offres"));
     }
 }
